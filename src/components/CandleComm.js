d2f4290af1f3f2a972891b9b8e555ddc6126eecc
Tatiana Johnson <Tatianarjo@gmail.com>
2022-03-10 18:20:11 -0600

    first commit

---
+++ src/components/CandleComm.js
@@ -0,0 +1,37 @@
+import { NavBar } from "./nav/NavBar";
+import React from "react"
+import "./CandleComm.css"
+
+export const CandleComm = () => (
+  <>
+  <h1>CandleComm</h1>
+  </>
+
+)
+
+
+{/* // export const CandleComm = () => (
+//     <>
+//     <Route
+//       render={() => {
+//         if (localStorage.getItem("candleComm_user")) {
+//           return (
+//             <>
+//               <NavBar />
+//               {/* <ApplicationViews /> */}
+//             </>
+//           );
+//         } else {
+//           return <Redirect to="/login" />;
+//         }
+//       }}
+//     />
+
+//     <Route path="/login">
+//       <Login />
+//     </Route>
+//     <Route path="/register">
+//       <Register />
+//     </Route>
+//   </>
+// ) */}
---
+++ src/components/nav/NavBar.js
@@ -0,0 +1,25 @@
+import React from "react"
+// import { Link } from "react-router-dom"
+import "./NavBar.css"
+
+// export const NavBar = (props) => {
+//     return (
+//         <ul className="navbar">
+//              {/* <li className="navbar__item">
+//                 <Link className="navbar__link" to="/home">Home</Link>
+//             </li>  */}
+//              <li className="navbar__item active">
+//                 <Link className="navbar__link" to="/">CandleComm</Link>
+//             </li>
+//             <li className="navbar__item">
+//                 <Link className="navbar__link" to="/motivations">Motivation</Link>
+//             </li>
+//             <li className="navbar__item">
+//                 <Link className="navbar__link" to="/moodTrackers">Mood Tracker</Link>
+//             </li>
+//             <li className="navbar__item">
+//                 <Link className="navbar__link" to="/supports">Support</Link>
+//             </li>
+//         </ul>
+//     )
+// }
